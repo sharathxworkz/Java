3b7617a28afb75a0026de13afd97d2ecc2213450
unknown <sharath77.xworkz@gmail.com>
2022-05-19 11:45:48 -0400

    All Programs till now

--- james-java/CFS/SwitchnumDemo.java
+++ james-java/CFS/SwitchnumDemo.java
@@ -10,17 +10,17 @@ class SwitchnumDemo
 
 		switch (num) {
 
-		  case 0: System.out.println("Entered Number is Zero");
+		  case 0: System.out.println("Zero");
 			    break;
-		  case 1: System.out.println("Entered Number is One");
+		  case 1: System.out.println("One");
 			    break;	
-	          case 2: System.out.println("Entered Number is Two");
+	          case 2: System.out.println("Two");
 			    break;
-		  case 3: System.out.println("Entered Number is Three");
+		  case 3: System.out.println("Three");
 			    break;
-		  case 4: System.out.println("Entered Number is Four");
+		  case 4: System.out.println("Four");
 			    break;
-		  case 5: System.out.println("Entered Number is Five");
+		  case 5: System.out.println("Five");
 			    break;
 		  default: System.out.println("Invalid Number");
 			  break; 
