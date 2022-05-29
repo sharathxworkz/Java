fda324c7a0474644c6652f272dab0409b07fe570
Shadow <sharath77.xworkz@gmail.com>
2022-05-29 12:23:20 -0400

    first code

---
+++ HelloWorld/src/com/example/Message.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("wecome to core java");
+	}
+
+}
