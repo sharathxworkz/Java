ba8dc8234f00c840cd561d67cbeaf144298fc534
Shadow <sharath77.xworkz@gmail.com>
2022-05-29 11:45:36 -0400

    message true

---
+++ Core_Java/src/com/xworkz/inheritance/SingleInheritance/Message.java
@@ -0,0 +1,14 @@
+package com.xworkz.inheritance.SingleInheritance;
+
+public class Message {
+
+	public Message() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome tom & jerry"); 
+	}
+
+}
