7d8792c6c4213e4ffe051f780f4157d6e5b8c8de
Shadow <sharath77.xworkz@gmail.com>
2022-05-30 16:08:39 -0400

    Inheritance Programs(S,M,h)

---
+++ Core_Java/src/com/xworkz/corejava/Inheritance/SimpleInheritance/Cow/Calf.java
@@ -0,0 +1,8 @@
+package com.xworkz.corejava.Inheritance.SimpleInheritance.Cow;
+
+public class Calf extends Cow {
+
+	void play() {
+		System.out.println("Calf is playing Outside Happy..!");
+	}
+}
---
+++ Core_Java/src/com/xworkz/corejava/Inheritance/SimpleInheritance/Cow/Cow.java
@@ -0,0 +1,11 @@
+package com.xworkz.corejava.Inheritance.SimpleInheritance.Cow;
+
+public class Cow {
+
+	void eat() {
+		System.out.println("Cow Eats Grass");
+	}
+	void ProduceMilk() {
+		System.out.println("Cow Produces Milk");
+	}
+}
