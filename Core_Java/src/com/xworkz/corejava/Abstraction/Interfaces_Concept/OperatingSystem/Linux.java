c88f86a23a99192329e5f5bf05514363a995fb10
Shadow <sharath77.xworkz@gmail.com>
2022-06-13 21:07:40 -0400

    updated Linux Class

--- Core_Java/src/com/xworkz/corejava/Abstraction/Interfaces_Concept/OperatingSystem/Linux.java
+++ Core_Java/src/com/xworkz/corejava/Abstraction/Interfaces_Concept/OperatingSystem/Linux.java
@@ -10,7 +10,8 @@ public class Linux implements MediaPlayer {
 	@Override
 	public void playMovie() {
 		// TODO Auto-generated method stub
-		System.out.println("Linux Uses SMplayer software to play Movies and Song Vedious..!!");
+		System.out.println("Linux"
+				+ " Uses SMplayer software to play Movies and Song Vedious..!!");
 	}
 
 	
