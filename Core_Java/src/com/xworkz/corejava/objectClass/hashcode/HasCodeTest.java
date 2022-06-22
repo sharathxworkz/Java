644836c219e7b7805c39acc661abb35750a2c2e9
Shadow <sharath77.xworkz@gmail.com>
2022-06-22 03:55:48 -0400

    Object_Class and Instance Of.

---
+++ Core_Java/src/com/xworkz/corejava/objectClass/hashcode/HasCodeTest.java
@@ -0,0 +1,12 @@
+package com.xworkz.corejava.objectClass.hashcode;
+
+public class HasCodeTest {
+
+	public static void main(String[] args) {
+
+		Pen pen = new Pen();
+		System.out.println("Pens hascode:"+pen.hashCode());
+
+	}
+
+}
---
+++ Core_Java/src/com/xworkz/corejava/objectClass/hashcode/Pen.java
@@ -0,0 +1,10 @@
+package com.xworkz.corejava.objectClass.hashcode;
+
+public class Pen {
+	
+	public int hashCode() {
+
+		System.out.println("Hash Code Overriden");
+		return 1;
+   }
+}
